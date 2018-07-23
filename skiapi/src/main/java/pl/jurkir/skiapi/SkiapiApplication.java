8893ceb876de598876608e1ce0966a8c1b86ec88
Jerzy Kiryczuk <203903@edu.p.lodz.pl>
2018-07-23 18:36:57 +0200

    Init add api project

---
+++ skiapi/src/main/java/pl/jurkir/skiapi/SkiapiApplication.java
@@ -0,0 +1,12 @@
+package pl.jurkir.skiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiapiApplication.class, args);
+	}
+}
