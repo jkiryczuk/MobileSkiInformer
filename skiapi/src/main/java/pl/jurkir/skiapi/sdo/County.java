cff95845f9038d19a7589a0f745586164fa5a6c5
Jerzy Kiryczuk <jurkirk@gmail.com>
2018-08-12 23:33:55 +0200

    * List view in SDO

--- skiapi/src/main/java/pl/jurkir/skiapi/sdo/County.java
+++ skiapi/src/main/java/pl/jurkir/skiapi/sdo/County.java
@@ -2,18 +2,23 @@ package pl.jurkir.skiapi.sdo;
 
 import lombok.Data;
 
-//TODO dodać listę gmin
+import java.util.ArrayList;
+import java.util.List;
+
+//TODO dodać listę gmin -- czy konieczne?
 @Data
 public class County {
 
     private Long id;
     private String name;
     private Voivodeship voivodeship;
+    private List<Borough> boroughs;
 
-    public County(Long id, String name, Voivodeship voivodeship) {
+    public County(Long id, String name, Voivodeship voivodeship, List<Borough> boroughs) {
         this.id = id;
         this.name = name;
         this.voivodeship = voivodeship;
+        this.boroughs = boroughs;
     }
 
     public County(pl.jurkir.skiapi.dao.County county) {
@@ -23,6 +28,14 @@ public class County {
         voivodeship.setId(county.getVoivodeship().getId());
         voivodeship.setName(county.getVoivodeship().getName());
         this.voivodeship = voivodeship;
+        List<Borough> boroughs = new ArrayList<>();
+        for (int i = 0; i < county.getBoroughs().size(); i++) {
+            Borough borough = new Borough();
+            borough.setId(county.getBoroughs().get(i).getId());
+            borough.setName(county.getBoroughs().get(i).getName());
+            boroughs.add(borough);
+        }
+        this.boroughs = boroughs;
     }
 
     public County() {
